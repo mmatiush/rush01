a3d97a34f4ceaa2232582bc9eedf3013c4bc83eb
Maksym MATIUSHCHENKO <mmatiush@e2r1p2.unit.ua>
2018-10-13 19:48:23 +0300

    main

---
+++ new_main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int		main(int ac, char **av) {
+	if (ac == 1)
+		;// ncurses.run();
+	if (ac == 2 && strcmp(av[1], "g"))
+		;// MLX.run();
+	else
+		std::cout << "Usage: ./ft_gkrellm [ g ]" << std::endl;
+}
