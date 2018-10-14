8d1108f03bc8f3a8f8fb0a1d9ceea34e6506e302
Yevhen Samchuk <evsamchuk@gmail.com>
2018-10-14 12:16:25 +0300

    MLXDisplay

--- MLXDisplay.hpp
+++ MLXDisplay.hpp
@@ -1,18 +1,38 @@
 #ifndef MLXDISPLAY_HPP
 # define MLXDISPLAY_HPP
-# include <iostream>
-# include <string>
+
 # include "IMonitorDisplay.hpp"
+# include "IMonitorModule.hpp"
+# include "OSInfo.hpp"
+# include "TimeModule.hpp"
+# include "HostUserNames.hpp"
+# include "RAM.hpp"
+# include "CPU.hpp"
+# include "Network.hpp"
+# include <vector>
 
 class MLXDisplay : public IMonitorDisplay
 {
 
+private:
+
+	std::vector<IMonitorModule *> _modules;
+
 public:
+
+	std::vector<IMonitorModule *> getModules() const;
 
 	MLXDisplay();
 	~MLXDisplay();
 	MLXDisplay(MLXDisplay const & other);
 	MLXDisplay & operator=(MLXDisplay const & other);
+
+	void	displayHostUserName( void );
+	void	displayNetwork( void );
+	void	displayCPU( void );
+	void	displayRAM( void );
+	void	displayOSinfo( void );
+	void	displayTime( void );
 
 };
 
